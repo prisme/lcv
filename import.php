4b53bd28d22034cc2d9b6b6f511a3760dc448c6f
prisme <jeremy.saintprix@gmail.com>
2017-09-11 18:15:52 +0200

    data import thing…

--- import.php
+++ import.php
@@ -1,14 +1,15 @@
 <?php 
 	require('cockpit/bootstrap.php'); // load cockpit api
 
-	// load and decode json to php array
-	// $data = json_decode(file_get_contents('spectacles.json'), true); // decode as array
-	// get collection and insert data
-	// collection('spectacles')->insert($data);
+	$data = json_decode(file_get_contents('import/spectacles.json'), true);
+	collection('spectacles')->insert($data);
 
-	// $data = json_decode(file_get_contents('ateliers.json'), true); // decode as array
-	// collection('ateliers')->insert($data);
+	$data = json_decode(file_get_contents('import/ateliers.json'), true);
+	collection('ateliers')->insert($data);
 
-	// $data = json_decode(file_get_contents('lcv.json'), true); // decode as array
-	// collection('lcv')->insert($data);
+	$data = json_decode(file_get_contents('import/lcv.json'), true);
+	collection('lcv')->insert($data);
+
+	$data = json_decode(file_get_contents('import/presse.json'), true);
+	collection('presse')->insert($data);
 ?>
